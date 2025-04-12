6ef153968986d9e4c8f1463babc21bdc2d82535c
Vivek Chavda <115214436+Vivekchavda1374@users.noreply.github.com>
2025-04-12 11:11:18 +0530

    impport lib

---
+++ main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+
+
